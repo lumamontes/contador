51d351fbb91d75850216460e498d13adc6ac561b
Luma Montes <lumagoesmontes@gmail.com>
2020-07-11 00:23:24 -0300

    adicionado o valor da data

---
+++ script.js
@@ -0,0 +1,7 @@
+    const botao = document.querySelector('#btn')
+    const data = document.querySelector('#data')
+    botao.addEventListener('click', iniciarContagem)
+    function iniciarContagem (){
+            console.log(data.value) 
+   }
+
